af87d637e8681fe1e3c2ce642e9f76a247f5cf83
Thando Ngwabi <thando10ngwabi@gmail.com>
2023-02-25 15:07:08 +0200

    initial

---
+++ src/MessierObject.java
@@ -0,0 +1,13 @@
+public class MessierObject {
+    public String messierNumber;
+    public String NGC;
+    public String name;
+    public  String type;
+    public Double distance;
+    public  String constellation;
+    public  String apparentMagnitude;
+    public Double rightAscension;
+    public Double leftAscension;
+    
+
+}
